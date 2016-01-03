5826db5360580dc17c5aa5a19d64f4a26d4dd4d7
Phil Coltrane <phil.m.coltrane@gmail.com>
2016-01-03 18:56:33 -0500

    put list into a whiteframe and centered

--- ts/app.ts
+++ ts/app.ts
@@ -1,7 +1,14 @@
 /// <reference path="../typings/tsd.d.ts" />
 'use strict';
 
-module PMC{
-    
-    export var appModule = angular.module('pmcApp', ['ngMaterial']);
+module PMC {
+
+    export var appModule = angular
+        .module('pmcApp', ['ngMaterial'])
+        .config(function($mdThemingProvider) {
+            $mdThemingProvider
+            .theme('default')
+            .primaryPalette('green')
+            .accentPalette('blue');
+        });
 }
